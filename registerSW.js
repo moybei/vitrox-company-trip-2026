c0beefe6924f2e1beb13f2db0d15b41116da990f
moybei <moybei@users.noreply.github.com>
2026-06-26 03:31:27 +0000

    deploy: 034b83c3f94781f3a8664268196d751a6f90d8e0

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vitrox-company-trip-2026/sw.js', { scope: '/vitrox-company-trip-2026/' })})}
